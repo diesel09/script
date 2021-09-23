0beec84eaf32b5c32769d7eb6a7abb8a17d73ae0
diesel09 <37728951+diesel09@users.noreply.github.com>
2021-09-22 23:10:56 -0500

    Update VPS-MX.sh

--- generadores/control/VPS-MX.sh
+++ generadores/control/VPS-MX.sh
@@ -122,11 +122,6 @@ echo -e "\033[97m    # apt-get install unzip........... $ESTATUS "
 #zip
 [[ $(dpkg --get-selections|grep -w "zip"|head -1) ]] || apt-get install zip -y &>/dev/null
 [[ $(dpkg --get-selections|grep -w "zip"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
-[[ $(dpkg --get-selections|grep -w "zip"|head -1) ] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
-echo -e "\033[97m    # apt-get install zip............. $ESTATUS "
-#zip
-[[ $(dpkg --get-selections|grep -w "zip"|head -1) ]] || apt-get install zip -y &>/dev/null
-[[ $(dpkg --get-selections|grep -w "zip"|head -1) ]] || ESTATUS=`echo -e "\033[91mFALLO DE INSTALACION"` &>/dev/null
 [[ $(dpkg --get-selections|grep -w "zip"|head -1) ]] && ESTATUS=`echo -e "\033[92mINSTALADO"` &>/dev/null
 #apache2
 [[ $(dpkg --get-selections|grep -w "apache2"|head -1) ]] || {
