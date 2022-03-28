37291ae71dd6800aa9d1fdd5ae18c8072dcb9d20
diesel09 <37728951+diesel09@users.noreply.github.com>
2022-03-28 00:54:05 -0500

    Update instgerador.sh

--- translate/instgerador.sh
+++ translate/instgerador.sh
@@ -46,7 +46,7 @@ echo -e "${lor4}****************************************${lor7}"
 echo -e
 echo -e "\033[97m   ◆ ACCESO NO AUTORIZADO CONTACTAME PARA ACCESOS"
 echo -e "\033[97m   ◆ TELEGRAM: @FelipeCouoh"
-echo -e "\033[97m   ◆ WHATSAPP: +52 982147259"
+echo -e "\033[97m   ◆ WHATSAPP: +52 9842147259"
 echo -e
 echo -e "${lor4}****************************************${lor7}"
 for pid in $(pgrep python);do
